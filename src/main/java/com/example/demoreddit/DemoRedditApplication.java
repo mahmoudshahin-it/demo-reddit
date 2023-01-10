047eaca79758c5236c910072e5f4ef5e4e3ed9d0
ittovative <mahmoudshahin.it@gmail.com>
2023-01-11 01:16:02 +0800

    Empty project - with key dependencies

---
+++ src/main/java/com/example/demoreddit/DemoRedditApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRedditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRedditApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoreddit/DemoRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoreddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRedditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
