88d367c1d64f7c41fcf53821ec9ad40071a2c741
ittovative <mahmoudshahin.it@gmail.com>
2023-01-11 02:55:53 +0800

    fix package name

--- src/main/java/com/ittovative/demoreddit/DemoRedditApplication.java
+++ src/main/java/com/ittovative/demoreddit/DemoRedditApplication.java
@@ -1,4 +1,4 @@
-package com.example.demoreddit;
+package com.ittovative.demoreddit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ittovative/demoreddit/DemoRedditApplicationTests.java
+++ src/test/java/com/ittovative/demoreddit/DemoRedditApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demoreddit;
+package com.ittovative.demoreddit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
